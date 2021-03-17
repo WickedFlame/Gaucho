292dcaaaa6bf51e982c6e0d84c13aec71000ea41
Christian Walpen <christianwalpen@opacc.ch>
2021-03-17 11:27:30 +0100

    Log the amount of events that are in the queue when creating a worker

--- src/Gaucho/EventBus.cs
+++ src/Gaucho/EventBus.cs
@@ -9,27 +9,9 @@ using Gaucho.Server.Monitoring;
 
 namespace Gaucho
 {
-    public interface IEventBus : IDisposable
-    {
-        string PipelineId { get; }
-
-        /// <summary>
-        /// gets the pipelinefactory that creates the pipeline for this eventbus
-        /// </summary>
-		IPipelineFactory PipelineFactory { get; }
-
-        void SetPipeline(IPipelineFactory factory);
-
-        void Publish(Event @event);
-
-        void Close();
-    }
-
-    public interface IAsyncEventBus : IEventBus
-    {
-        void WaitAll();
-    }
-
+	/// <summary>
+	/// 
+	/// </summary>
     public class EventBus : IAsyncEventBus, IEventBus
     {
         private readonly object _syncRoot = new object();
@@ -42,11 +24,21 @@ namespace Gaucho
         private readonly List<EventProcessor> _processors = new List<EventProcessor>();
         private int _minThreads = 1;
 
-        public EventBus(Func<IEventPipeline> factory, string pipelineId)
+		/// <summary>
+		/// Creates a new instance of the eventbus
+		/// </summary>
+		/// <param name="factory"></param>
+		/// <param name="pipelineId"></param>
+		public EventBus(Func<IEventPipeline> factory, string pipelineId)
             : this(new PipelineFactory(factory), pipelineId)
         {
         }
 
+		/// <summary>
+		/// Creates a new instance of the eventbus
+		/// </summary>
+		/// <param name="pipelineFactory"></param>
+		/// <param name="pipelineId"></param>
         public EventBus(IPipelineFactory pipelineFactory, string pipelineId)
             : this(pipelineId)
         {
@@ -74,14 +66,19 @@ namespace Gaucho
             SetupProcessors(1);
         }
 
+		/// <summary>
+		/// Gets the PipelineId
+		/// </summary>
         public string PipelineId { get; }
 
 		/// <summary>
-		/// gets the pipelinefactory that creates the pipeline for this eventbus
+		/// Gets the pipelinefactory that creates the pipeline for this eventbus
 		/// </summary>
         public IPipelineFactory PipelineFactory => _pipelineFactory;
 
-
+		/// <summary>
+		/// Waits for all processors to end the work
+		/// </summary>
 		public void WaitAll()
         {
             var tasks = _processors.Select(t => t.Task)
@@ -94,23 +91,35 @@ namespace Gaucho
             }
         }
 
+		/// <summary>
+		/// The pipeline will be closed after all queued events are processed.
+		/// </summary>
         public void Close()
         {
 	        _minThreads = 0;
         }
 
+		/// <summary>
+		/// Set the pipelinefactory
+		/// </summary>
+		/// <param name="factory"></param>
         public void SetPipeline(IPipelineFactory factory)
         {
             _pipelineFactory = factory;
         }
 
+		/// <summary>
+		/// Publish an event to the processingqueue
+		/// </summary>
+		/// <param name="event"></param>
         public void Publish(Event @event)
         {
             _queue.Enqueue(@event);
 
             if (_queue.Count / _processors.Count > 10)
             {
-                SetupProcessors(_processors.Count + 1);
+	            _logger.Write($"Items in Queue count: {_queue.Count}", Category.Log, source: "EventBus");
+				SetupProcessors(_processors.Count + 1);
             }
 
             foreach (var process in _processors.ToList())
@@ -119,6 +128,10 @@ namespace Gaucho
             }
         }
 
+		/// <summary>
+		/// Start porcessing the pipeline
+		/// </summary>
+		/// <param name="pipeline"></param>
         public void Process(IEventPipeline pipeline)
         {
 	        _logger.Write("Begin processing events", Category.Log, LogLevel.Debug, "EventBus");
